9012b8bd1c59e46448a9b25d5a9852e331bbc7d4
Daniil <alexamyrz@yandex.ru>
2026-04-16 15:30:57 +0300

    VERY BIG COMMIT

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,6 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    // идентификатор плагина Google Services, который нужен Firebase, чтобы обработать файл google-services.json
+    id("com.google.gms.google-services") version "4.4.4" apply false
 }
